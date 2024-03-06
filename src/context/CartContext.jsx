e00b6881311cfa10c256b01956b786a5feb26729
Jefferson Ore <jeffer.ore@gmail.com>
2024-03-06 16:44:06 -0500

    modificando

--- src/context/CartContext.jsx
+++ src/context/CartContext.jsx
@@ -46,7 +46,7 @@ export const CartProvider = ({children}) => {
                             allowEscapeKey: false,
                             html: `Producto sin Stock : <strong>${snapshot.data().nombre}</strong> </br> Stock maximo permitido: <strong>${snapshot.data().stock}</strong> </br>
                                    Ya tienes agregado el producto <strong>${snapshot.data().nombre}</strong> en el carrito, con la cantidad de : <strong>${newProduct[productIndex].cantidad}</strong>.</br>
-                                   ${(snapshot.data().stock - (newProduct[productIndex].cantidad + count)) < 0 ? 'La cantidad perimitada para agregar es : <strong>' + (snapshot.data().stock - (newProduct[productIndex].cantidad)) + '</strong>' : '' } `
+                                   ${(snapshot.data().stock - (newProduct[productIndex].cantidad + count)) < 0 ? 'La cantidad permitada para agregar es : <strong>' + (snapshot.data().stock - (newProduct[productIndex].cantidad)) + '</strong>' : '' } `
                         })
                     }
                 }
